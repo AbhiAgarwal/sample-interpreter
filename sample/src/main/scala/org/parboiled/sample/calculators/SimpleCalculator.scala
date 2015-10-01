acf7df07f130f18ea6a1622e832d6a54c1057814
Abhi Agarwal <me@abhiagarwal.com>
2015-10-01 16:29:54 -0400

    Fixing up to return an answer

--- sample/src/main/scala/org/parboiled/sample/calculators/SimpleCalculator.scala
+++ sample/src/main/scala/org/parboiled/sample/calculators/SimpleCalculator.scala
@@ -1,18 +1,38 @@
 import org.parboiled.scala._
+import org.parboiled.errors.{ErrorUtils, ParsingException}
 
 class SimpleCalculator extends Parser {
-  def Expression: Rule0 = rule { Term ~ zeroOrMore(anyOf("+-") ~ Term) }
-  def Term = rule { Factor ~ zeroOrMore(anyOf("*/") ~ Factor) }
-  def Factor = rule { Number | "(" ~ Expression ~ ")" }
-  def Number = rule { oneOrMore("0" - "9") }
+  def InputLine = rule { Expression ~ EOI }
+  def Expression: Rule1[Int] = rule {
+    Term ~ zeroOrMore(
+      "+" ~ Term ~~> ((a:Int, b) => a + b)
+    | "-" ~ Term ~~> ((a:Int, b) => a - b)
+    )
+  }
+  def Term = rule {
+    Factor ~ zeroOrMore(
+      "*" ~ Factor ~~> ((a:Int, b) => a * b)
+    | "/" ~ Factor ~~> ((a:Int, b) => a / b)
+    )
+  }
+  def Factor = rule { Number | Parens }
+  def Parens = rule { "(" ~ Expression ~ ")" }
+  def Number = rule { Digits ~> (_.toInt) }
+  def Digits = rule { oneOrMore(Digit) }
+  def Digit = rule { "0" - "9" }
 }
 
 object RunSimpleCalculator {
   def main(args: Array[String]) {
-    val input = "1+2"
+    val input = "(1+2+3) + 4"
     val parser = new SimpleCalculator { override val buildParseTree = true }
-    val result = ReportingParseRunner(parser.Expression).run(input)
-    val parseTreePrintOut = org.parboiled.support.ParseTreeUtils.printNodeTree(result)
-    println(parseTreePrintOut)
+    val parsingResult = ReportingParseRunner(parser.Expression).run(input)
+    parsingResult.result match {
+      case Some(i) => println("Answer is: " + i)
+      case None => throw new ParsingException("Invalid calculation expression:\n" +
+              ErrorUtils.printParseErrors(parsingResult)) 
+    }
+    // val parseTreePrintOut = org.parboiled.support.ParseTreeUtils.printNodeTree(parsingResult)
+    // println(parseTreePrintOut)
   }
 }
